e004443d859147bf947c9ec9d32c703c6adeac49
Rick Bergfalk <rick.bergfalk@gmail.com>
2014-03-06 19:44:36 -0600

    test script updates

--- test/test-postgrator.js
+++ test/test-postgrator.js
@@ -9,6 +9,9 @@ var async = require('async');
 
 var tests = [];
 
+var pgUrl = "tcp://qblgodnjwwvqjr:auWYSDIW73KC1scgGv-VDquQGJ@ec2-54-204-24-154.compute-1.amazonaws.com/d42c6mk8cotcn2";
+
+process.env.PGSSLMODE = 'require';
 
 /* Test original API to make sure its compatible
    We're going up and back down because we want to leave the database in a clean state for other tests
@@ -17,7 +20,7 @@ tests.push(function (callback) {
 	console.log('\n----- testing original api to 003 -----');
 	var postgrator = require('../postgrator.js');
 	postgrator.setMigrationDirectory(__dirname + '/migrations');
-	postgrator.setConnectionString("tcp://rickber2_test:TestUser@just63.justhost.com/rickber2_test"); 
+	postgrator.setConnectionString(pgUrl); 
 	postgrator.migrate('003', function(err, migrations) {
 		assert.ifError(err);
 		callback();
@@ -28,7 +31,7 @@ tests.push(function (callback) {
 	console.log('\n----- testing original api to 000 -----');
 	var postgrator = require('../postgrator.js');
 	postgrator.setMigrationDirectory(__dirname + '/migrations');
-	postgrator.setConnectionString("tcp://rickber2_test:TestUser@just63.justhost.com/rickber2_test"); 
+	postgrator.setConnectionString(pgUrl); 
 	postgrator.migrate('000', function(err, migrations) {
 		assert.ifError(err);
 		callback();
@@ -135,28 +138,26 @@ var buildTestsForConfig = function (config) {
 buildTestsForConfig({
 	migrationDirectory: __dirname + '/migrations',
 	driver: 'pg',
-	host: 'just63.justhost.com',
-	database: 'rickber2_test',
-	username: 'rickber2_test',
-	password: 'TestUser'
+	host: 'ec2-54-204-24-154.compute-1.amazonaws.com',
+	database: 'd42c6mk8cotcn2',
+	username: 'qblgodnjwwvqjr',
+	password: 'auWYSDIW73KC1scgGv-VDquQGJ'
 });
-
 buildTestsForConfig({
 	migrationDirectory: __dirname + '/migrations',
 	driver: 'mysql',
-	host: 'just63.justhost.com',
-	database: 'rickber2_test',
-	username: 'rickber2_test',
-	password: 'TestUser'
+	host: 'blue2.corecloud.com',
+	database: 'test',
+	username: 'testuser',
+	password: 'testuser'
 });
-
 buildTestsForConfig({
 	migrationDirectory: __dirname + '/migrations',
 	driver: 'tedious',
 	host: '127.0.0.1',
-	database: 'myUtility',
-	username: 'rickber2_test',
-	password: 'TestUser1!'
+	database: 'Utility',
+	username: 'testuser',
+	password: 'testuser'
 });
 
 	
@@ -171,6 +172,3 @@ async.eachSeries(tests, function(testFunc, callback) {
 	assert.ifError(err); // this won't ever happen, as we don't pass errors on in our test. But just in case we do some day...
 	console.log('\nEverythings gonna be alright');
 });
-
-
-
