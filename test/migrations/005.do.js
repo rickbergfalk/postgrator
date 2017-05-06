77be662f14241a774c8d3eb6c32aa37b409b146f
Sethu <writetosethu@gmail.com>
2017-05-07 02:39:44 +1000

    Cleaned up the test a bit by removing additional process.env.

--- test/migrations/005.do.js
+++ test/migrations/005.do.js
@@ -1,7 +1,3 @@
-/*
-  using this to demo that you use environment variables to generate sql
- */
-process.env.TEST_NAME = 'aesthete';
 
 module.exports.generateSql = function () {
   return "INSERT INTO person (name, age) VALUES ('"+process.env.TEST_NAME+"', 21);"
--- test/migrations/005.undo.js
+++ test/migrations/005.undo.js
@@ -1,7 +1,3 @@
-/*
-  using this to demo that you use environment variables to generate sql
- */
-process.env.TEST_NAME = 'aesthete';
 
 module.exports.generateSql = function () {
   return "DELETE FROM person where name = '"+process.env.TEST_NAME+"'";
