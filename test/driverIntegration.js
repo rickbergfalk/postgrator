7952ece9a4538b54229c913420a8f690f7909457
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-11-18 14:40:44 -0500

    Fix implementation for SQL Server

--- test/driverIntegration.js
+++ test/driverIntegration.js
@@ -25,14 +25,14 @@ testConfig({
 })
 
 // SQL Server needs 3.25 GB of RAM and for some reason doesn't download...
-// testConfig({
-//   migrationDirectory: migrationDirectory,
-//   driver: 'mssql',
-//   host: 'localhost',
-//   database: 'master',
-//   username: 'sa',
-//   password: 'Postgrator123!'
-// })
+testConfig({
+  migrationDirectory: migrationDirectory,
+  driver: 'mssql',
+  host: 'localhost',
+  database: 'master',
+  username: 'sa',
+  password: 'Postgrator123!'
+})
 
 function testConfig(config) {
   describe(`Driver: ${config.driver}`, function() {
