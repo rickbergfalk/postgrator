9cf55b3b8260a8b771637a0460a08ee53a052a21
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-11-18 15:27:44 -0500

    Comment out SQL Server docker/testing

--- test/driverIntegration.js
+++ test/driverIntegration.js
@@ -24,15 +24,15 @@ testConfig({
   password: 'postgrator'
 })
 
-// SQL Server needs 3.25 GB of RAM and for some reason doesn't download...
-testConfig({
-  migrationDirectory: migrationDirectory,
-  driver: 'mssql',
-  host: 'localhost',
-  database: 'master',
-  username: 'sa',
-  password: 'Postgrator123!'
-})
+// SQL Server needs 3.25 GB of RAM
+// testConfig({
+//   migrationDirectory: migrationDirectory,
+//   driver: 'mssql',
+//   host: 'localhost',
+//   database: 'master',
+//   username: 'sa',
+//   password: 'Postgrator123!'
+// })
 
 function testConfig(config) {
   describe(`Driver: ${config.driver}`, function() {
