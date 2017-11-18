d839b0981729a66c64adaa02b1ef4ad26354615b
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-11-18 15:27:23 -0500

    Add comment about BEGIN/END

--- test/failMigrations/002.do.fail-test.sql
+++ test/failMigrations/002.do.fail-test.sql
@@ -1,3 +1,6 @@
+-- NOTE without BEGIN/END you could end up with partial implemented migration, which is bad
+-- Postgres and SQL Server implicitly wrap 1 multi-statement execution in a transaction by default
+-- MySQL however does not, and you are left with partial migration implemented
 BEGIN
     INSERT INTO widgets (name) VALUES ('widget one');
     INSERT INTO widgets (name) VALUES ('widget two');
