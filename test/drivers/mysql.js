6589674844cba1bbddb908dae3540709e093ab6b
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-21 12:56:24 -0500

    Consider schema/database in ensureTable check (#49)
    * Add schema to ensureTable check
    
    * Handle config.database not be specified (pg connection string)
    
    * Split tests up

---
+++ test/drivers/mysql.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const testConfig = require('./driverIntegration')
+
+testConfig({
+  migrationDirectory: path.join(__dirname, '../migrations'),
+  driver: 'mysql',
+  host: 'localhost',
+  database: 'postgrator',
+  username: 'postgrator',
+  password: 'postgrator'
+})
