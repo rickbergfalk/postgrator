7d833e5832faae5d140fd94632ffe56cf0b13708
Rick Bergfalk <rbergfalk@uptycs.com>
2018-10-13 14:44:09 -0400

    Add a mysql2 against mysql 8 test

--- test/drivers/mysql2.js
+++ test/drivers/mysql2.js
@@ -4,12 +4,15 @@ const testConfig = require('./driverIntegration')
 testConfig(
   {
     migrationDirectory: path.join(__dirname, '../migrations'),
-    driver: 'mysql',
-    port: 3306,
+    driver: 'mysql2',
+    // MySQL 8 is mapped to different port
+    // TODO - test code should probably run in container so port mapping is unnecessary
+    // and db can be targeted by host alone
+    port: 3008,
     host: 'localhost',
     database: 'postgrator',
     username: 'postgrator',
     password: 'postgrator'
   },
-  'Driver: mysql against MariaDB'
+  'Driver: mysql2 against MySQL 8'
 )
