a636982697f1384d163940f47f04aac523e09b56
Rick Bergfalk <rbergfalk@uptycs.com>
2018-10-13 15:16:26 -0400

    just test mysql2

--- test/drivers/mysql.js
+++ test/drivers/mysql.js
@@ -1,15 +1,11 @@
 const path = require('path')
 const testConfig = require('./driverIntegration')
 
-testConfig(
-  {
-    migrationDirectory: path.join(__dirname, '../migrations'),
-    driver: 'mysql',
-    port: 3306,
-    host: 'localhost',
-    database: 'postgrator',
-    username: 'postgrator',
-    password: 'postgrator'
-  },
-  'Driver: mysql against MariaDB'
-)
+testConfig({
+  migrationDirectory: path.join(__dirname, '../migrations'),
+  driver: 'mysql',
+  host: 'localhost',
+  database: 'postgrator',
+  username: 'postgrator',
+  password: 'postgrator'
+})
--- test/drivers/mysql2.js
+++ test/drivers/mysql2.js
@@ -1,18 +1,11 @@
 const path = require('path')
 const testConfig = require('./driverIntegration')
 
-testConfig(
-  {
-    migrationDirectory: path.join(__dirname, '../migrations'),
-    driver: 'mysql2',
-    // MySQL 8 is mapped to different port
-    // TODO - test code should probably run in container so port mapping is unnecessary
-    // and db can be targeted by host alone
-    port: 3008,
-    host: 'localhost',
-    database: 'postgrator',
-    username: 'postgrator',
-    password: 'postgrator'
-  },
-  'Driver: mysql2 against MySQL 8'
-)
+testConfig({
+  migrationDirectory: path.join(__dirname, '../migrations'),
+  driver: 'mysql2',
+  host: 'localhost',
+  database: 'postgrator',
+  username: 'postgrator',
+  password: 'postgrator'
+})
