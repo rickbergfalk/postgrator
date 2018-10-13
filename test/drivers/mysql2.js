554ac707a41e2a8c5330e3439d33ac5f7304384d
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-10-13 15:22:08 -0400

    mysql2 support (tests and fixes) (#70)
    Fixes support for mysql2 use and adds tests.

---
+++ test/drivers/mysql2.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const testConfig = require('./driverIntegration')
+
+testConfig({
+  migrationDirectory: path.join(__dirname, '../migrations'),
+  driver: 'mysql2',
+  host: 'localhost',
+  database: 'postgrator',
+  username: 'postgrator',
+  password: 'postgrator'
+})
