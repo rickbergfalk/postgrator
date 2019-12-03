bf331d68571408dcedb2741c1b8a35beb6b7025f
Rick Bergfalk <rbergfalk@uptycs.com>
2019-12-02 19:46:01 -0600

    Support mysql2 version 2

--- test/utils.js
+++ test/utils.js
@@ -7,6 +7,7 @@ describe('getMajorVersion', function() {
     assert.strictEqual(utils.getMajorVersion('pg'), 7)
     assert.strictEqual(utils.getMajorVersion('mssql'), 6)
     assert.strictEqual(utils.getMajorVersion('mysql'), 2)
+    assert.strictEqual(utils.getMajorVersion('mysql2'), 2)
   })
 
   it('Fails gracefully for uninstalled module', function() {
