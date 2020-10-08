a73136c49cc880a4f03c92b5c1c8198b0a74691d
Luis Faustino <luis.faustino@nearform.com>
2020-10-08 10:12:49 +0100

    refactor migration file into async; README update;

--- test/migrations/006.do.js
+++ test/migrations/006.do.js
@@ -1,4 +1,8 @@
-module.exports.generateSql = function () {
+const { promisify } = require('util')
+const sleep = promisify(setTimeout)
+
+module.exports.generateSql = async () => {
+  await sleep(50)
   return (
     "INSERT INTO person (name, age) VALUES ('" +
     process.env.TEST_ANOTHER_NAME +
