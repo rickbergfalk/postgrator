28d04cc3b4eb4726ddbd7d9bb266c498702c773f
Rick Bergfalk <rick.bergfalk@gmail.com>
2021-09-09 00:14:43 -0500

    Use async-await (#126)
    * Use async await
    
    * Use updated mssql image

--- postgrator.js
+++ postgrator.js
@@ -16,6 +16,24 @@ const DEFAULT_CONFIG = {
   validateChecksums: true,
 }
 
+function loadMigrationDirOrPath(migrationDirectory, migrationPattern) {
+  return new Promise((resolve, reject) => {
+    const loader = (err, files) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(files)
+    }
+    if (migrationPattern) {
+      glob(migrationPattern, loader)
+    } else if (migrationDirectory) {
+      fs.readdir(migrationDirectory, loader)
+    } else {
+      resolve([])
+    }
+  })
+}
+
 class Postgrator extends EventEmitter {
   constructor(config) {
     super()
@@ -29,91 +47,78 @@ class Postgrator extends EventEmitter {
    *
    * @returns {Promise} array of migration objects
    */
-  getMigrations() {
+  async getMigrations() {
     const { migrationDirectory, migrationPattern, newline } = this.config
-    return new Promise((resolve, reject) => {
-      const loader = (err, files) => {
-        if (err) {
-          return reject(err)
-        }
-        resolve(files)
-      }
-      if (migrationPattern) {
-        glob(migrationPattern, loader)
-      } else if (migrationDirectory) {
-        fs.readdir(migrationDirectory, loader)
-      } else {
-        resolve([])
-      }
-    })
-      .then((migrationFiles) => {
-        return Promise.all(migrationFiles
-          .filter((file) => ['.sql', '.js'].indexOf(path.extname(file)) >= 0)
-          .map(async (file) => {
-            const basename = path.basename(file)
-            const ext = path.extname(basename)
+    const migrationFiles = await loadMigrationDirOrPath(
+      migrationDirectory,
+      migrationPattern
+    )
+    let migrations = await Promise.all(
+      migrationFiles
+        .filter((file) => ['.sql', '.js'].indexOf(path.extname(file)) >= 0)
+        .map(async (file) => {
+          const basename = path.basename(file)
+          const ext = path.extname(basename)
 
-            const basenameNoExt = path.basename(file, ext)
-            let [version, action, name = ''] = basenameNoExt.split('.')
-            version = Number(version)
+          const basenameNoExt = path.basename(file, ext)
+          let [version, action, name = ''] = basenameNoExt.split('.')
+          version = Number(version)
 
-            const filename = migrationPattern
-              ? file
-              : path.join(migrationDirectory, file)
+          const filename = migrationPattern
+            ? file
+            : path.join(migrationDirectory, file)
 
-            // TODO normalize filename on returned migration object
-            // Today it is full path if glob is used, otherwise basename with extension
-            // This is not persisted in the database, but this field might be a part of someone's workflow
-            // Making this change will be a breaking fix
+          // TODO normalize filename on returned migration object
+          // Today it is full path if glob is used, otherwise basename with extension
+          // This is not persisted in the database, but this field might be a part of someone's workflow
+          // Making this change will be a breaking fix
 
-            if (ext === '.sql') {
-              return {
-                version,
-                action,
-                filename: file,
-                name,
-                md5: fileChecksum(filename, newline),
-                getSql: () => fs.readFileSync(filename, 'utf8'),
-              }
+          if (ext === '.sql') {
+            return {
+              version,
+              action,
+              filename: file,
+              name,
+              md5: fileChecksum(filename, newline),
+              getSql: () => fs.readFileSync(filename, 'utf8'),
             }
-
-            if (ext === '.js') {
-              const jsModule = require(filename)
-              const sql = await jsModule.generateSql()
-
-              return {
-                version,
-                action,
-                filename: file,
-                name,
-                md5: checksum(sql, newline),
-                getSql: () => sql,
-              }
-            }
-          })
-      )})
-      .then((migrations) =>
-        migrations.filter((migration) => !isNaN(migration.version))
-      )
-      .then((migrations) => {
-        const getMigrationKey = (migration) =>
-          `${migration.version}:${migration.action}`
-        const migrationKeys = new Set()
-        migrations.forEach((migration) => {
-          const newKey = getMigrationKey(migration)
-          if (migrationKeys.has(newKey)) {
-            throw new Error(
-              `Two migrations found with version ${migration.version} and action ${migration.action}`
-            )
           }
-          migrationKeys.add(newKey)
+
+          if (ext === '.js') {
+            const jsModule = require(filename)
+            const sql = await jsModule.generateSql()
+
+            return {
+              version,
+              action,
+              filename: file,
+              name,
+              md5: checksum(sql, newline),
+              getSql: () => sql,
+            }
+          }
         })
-        return migrations
-      })
-      .then((migrations) => {
-        this.migrations = migrations
-        return migrations
-      })
+    )
+
+    migrations = migrations.filter((migration) => !isNaN(migration.version))
+
+    const getMigrationKey = (migration) =>
+      `${migration.version}:${migration.action}`
+
+    const migrationKeys = new Set()
+
+    migrations.forEach((migration) => {
+      const newKey = getMigrationKey(migration)
+      if (migrationKeys.has(newKey)) {
+        throw new Error(
+          `Two migrations found with version ${migration.version} and action ${migration.action}`
+        )
+      }
+      migrationKeys.add(newKey)
+    })
+
+    this.migrations = migrations
+    return migrations
   }
 
   /**
@@ -122,11 +127,11 @@ class Postgrator extends EventEmitter {
    * @returns {Promise} result of query
    * @param {String} query sql query to execute
    */
-  runQuery(query) {
+  async runQuery(query) {
     const { commonClient } = this
-    return commonClient.runQuery(query).then((results) => {
-      return commonClient.endConnection().then(() => results)
-    })
+    const results = await commonClient.runQuery(query)
+    await commonClient.endConnection()
+    return results
   }
 
   /**
@@ -135,12 +140,12 @@ class Postgrator extends EventEmitter {
    *
    * @returns {Promise} database schema version
    */
-  getDatabaseVersion() {
+  async getDatabaseVersion() {
     const versionSql = this.commonClient.getDatabaseVersionSql()
-    return this.commonClient.runQuery(versionSql).then((result) => {
-      const version = result.rows.length > 0 ? result.rows[0].version : 0
-      return this.commonClient.endConnection().then(() => parseInt(version))
-    })
+    const result = await this.commonClient.runQuery(versionSql)
+    const version = result.rows.length > 0 ? result.rows[0].version : 0
+    await this.commonClient.endConnection()
+    return parseInt(version)
   }
 
   /**
@@ -148,20 +153,13 @@ class Postgrator extends EventEmitter {
    *
    * @returns {Promise}
    */
-  getMaxVersion() {
-    const { migrations } = this
-    return Promise.resolve()
-      .then(() => {
-        if (migrations.length) {
-          return migrations
-        } else {
-          return this.getMigrations()
-        }
-      })
-      .then((migrations) => {
-        const versions = migrations.map((migration) => migration.version)
-        return Math.max.apply(null, versions)
-      })
+  async getMaxVersion() {
+    let { migrations } = this
+    if (!this.migrations.length) {
+      migrations = await this.getMigrations()
+    }
+    const versions = migrations.map((migration) => migration.version)
+    return Math.max.apply(null, versions)
   }
 
   /**
@@ -170,34 +168,29 @@ class Postgrator extends EventEmitter {
    * @returns {Promise}
    * @param {Number} databaseVersion
    */
-  validateMigrations(databaseVersion) {
-    return this.getMigrations().then((migrations) => {
-      const validateMigrations = migrations.filter(
-        (migration) =>
-          migration.action === 'do' &&
-          migration.version > 0 &&
-          migration.version <= databaseVersion
-      )
+  async validateMigrations(databaseVersion) {
+    const migrations = await this.getMigrations()
 
-      let sequence = Promise.resolve()
-      validateMigrations.forEach((migration) => {
-        sequence = sequence
-          .then(() => this.emit('validation-started', migration))
-          .then(() => {
-            const sql = this.commonClient.getMd5Sql(migration)
-            return this.commonClient.runQuery(sql)
-          })
-          .then((results) => {
-            const md5 = results.rows && results.rows[0] && results.rows[0].md5
-            if (md5 !== migration.md5) {
-              const msg = `MD5 checksum failed for migration [${migration.version}]`
-              throw new Error(msg)
-            }
-          })
-          .then(() => this.emit('validation-finished', migration))
-      })
-      return sequence.then(() => validateMigrations)
-    })
+    const validateMigrations = migrations.filter(
+      (migration) =>
+        migration.action === 'do' &&
+        migration.version > 0 &&
+        migration.version <= databaseVersion
+    )
+
+    for (const migration of validateMigrations) {
+      this.emit('validation-started', migration)
+      const sql = this.commonClient.getMd5Sql(migration)
+      const results = await this.commonClient.runQuery(sql)
+      const md5 = results.rows && results.rows[0] && results.rows[0].md5
+      if (md5 !== migration.md5) {
+        const msg = `MD5 checksum failed for migration [${migration.version}]`
+        throw new Error(msg)
+      }
+      this.emit('validation-finished', migration)
+    }
+
+    return validateMigrations
   }
 
   /**
@@ -206,27 +199,23 @@ class Postgrator extends EventEmitter {
    * @returns {Promise} - Array of migration objects to appled to database
    * @param {Array} migrations - Array of migration objects to apply to database
    */
-  runMigrations(migrations = []) {
+  async runMigrations(migrations = []) {
     const { commonClient } = this
-    let sequence = Promise.resolve()
     const appliedMigrations = []
-    migrations.forEach((migration) => {
-      sequence = sequence
-        .then(() => this.emit('migration-started', migration))
-        .then(() => migration.getSql())
-        .then((sql) => commonClient.runQuery(sql))
-        .then(() =>
-          commonClient.runQuery(commonClient.persistActionSql(migration))
-        )
-        .then(() => appliedMigrations.push(migration))
-        .then(() => this.emit('migration-finished', migration))
-    })
-    return sequence
-      .then(() => appliedMigrations)
-      .catch((error) => {
-        error.appliedMigrations = appliedMigrations
-        throw error
-      })
+    try {
+      for (const migration of migrations) {
+        this.emit('migration-started', migration)
+        const sql = await migration.getSql()
+        await commonClient.runQuery(sql)
+        await commonClient.runQuery(commonClient.persistActionSql(migration))
+        appliedMigrations.push(migration)
+        this.emit('migration-finished', migration)
+      }
+    } catch (error) {
+      error.appliedMigrations = appliedMigrations
+      throw error
+    }
+    return appliedMigrations
   }
 
   /**
@@ -269,52 +258,49 @@ class Postgrator extends EventEmitter {
    * @returns {Promise}
    * @param {String} target - version to migrate as string or number (handled as  numbers internally)
    */
-  migrate(target = '') {
+  async migrate(target = '') {
     const { commonClient, config } = this
     const data = {}
-    return commonClient
-      .ensureTable()
-      .then(() => this.getMigrations())
-      .then(() => {
-        const cleaned = target.toLowerCase().trim()
-        if (cleaned === 'max' || cleaned === '') {
-          return this.getMaxVersion()
-        }
-        return Number(target)
-      })
-      .then((targetVersion) => {
-        data.targetVersion = targetVersion
-        if (target === undefined) {
-          throw new Error('targetVersion undefined')
-        }
-        return this.getDatabaseVersion()
-      })
-      .then((databaseVersion) => {
-        data.databaseVersion = databaseVersion
-        if (config.validateChecksums && data.targetVersion >= databaseVersion) {
-          return this.validateMigrations(databaseVersion)
-        }
-      })
-      .then(() =>
-        this.getRunnableMigrations(data.databaseVersion, data.targetVersion)
+    try {
+      await commonClient.ensureTable()
+      await this.getMigrations()
+      let targetVersion
+      const cleaned = target.toLowerCase().trim()
+      if (cleaned === 'max' || cleaned === '') {
+        targetVersion = await this.getMaxVersion()
+      } else {
+        targetVersion = Number(target)
+      }
+      data.targetVersion = targetVersion
+      if (target === undefined) {
+        throw new Error('targetVersion undefined')
+      }
+      const databaseVersion = await this.getDatabaseVersion()
+      data.databaseVersion = databaseVersion
+      if (config.validateChecksums && data.targetVersion >= databaseVersion) {
+        await this.validateMigrations(databaseVersion)
+      }
+      const runnableMigrations = await this.getRunnableMigrations(
+        data.databaseVersion,
+        data.targetVersion
       )
-      .then((runnableMigrations) => this.runMigrations(runnableMigrations))
-      .then((migrations) => commonClient.endConnection().then(() => migrations))
-      .catch((error) => {
-        // Decorate error with empty appliedMigrations if not yet exist
-        // Rethrow error to module user
-        if (!error.appliedMigrations) {
-          error.appliedMigrations = []
-        }
-
-        // Attempt to close connection then throw original error
-        return commonClient
-          .endConnection()
-          .catch(() => {})
-          .then(() => {
-            throw error
-          })
-      })
+      const migrations = await this.runMigrations(runnableMigrations)
+      await commonClient.endConnection()
+      return migrations
+    } catch (error) {
+      // Decorate error with empty appliedMigrations if not yet exist
+      // Rethrow error to module user
+      if (!error.appliedMigrations) {
+        error.appliedMigrations = []
+      }
+      // Attempt to close connection then throw original error
+      try {
+        await commonClient.endConnection()
+      } catch (error) {
+        // no op
+      }
+      throw error
+    }
   }
 }
 
