135198cd68b68bd9a578074556243cdc8ae8a312
Rick Bergfalk <rick.bergfalk@gmail.com>
2022-03-31 23:09:06 -0500

    Did not mean to commit this

--- dirtest.js
+++
@@ -1,8 +0,0 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-console.log(import.meta.url);
-console.log(fileURLToPath(import.meta.url));
-console.log(__dirname);
